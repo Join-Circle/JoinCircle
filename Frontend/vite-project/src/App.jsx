3396e12512d976a4698ea01b0dfcfe689bcf822b
lavstart <lawtripathi49@gmail.com>
2025-06-12 14:32:08 +0530

    h

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -1,18 +1,9 @@
-// import Navbar from "./components/Navbar";
-// import BackgroundSection from "./components/BackGroundSection";
-// import PopularCommunities from "./components/PopularCommunities";
 import AboutSection from "./components/AboutSection";
-// import Footer from "./components/Footer";
 
 export default function HomePage() {
   return (
     <div className="text-gray-900 dark:text-white">
-      {/* <Navbar />
-     <BackgroundSection />
-
-      <PopularCommunities /> */}
       <AboutSection />
-      {/* <Footer /> */}
     </div>
   );
 }
