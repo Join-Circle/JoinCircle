0c3cd07399073d46df901c354df71af06c75beba
RanbirChauhan <ranbir21.2005@gmail.com>
2025-06-12 18:00:21 +0530

    footer1

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -1,9 +1,13 @@
+import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
+import Footer from "./components/Footer";
 
 export default function HomePage() {
   return (
     <div className="text-gray-900 dark:text-white">
+      <Navbar />
       <AboutSection />
+      <Footer />
     </div>
   );
 }
