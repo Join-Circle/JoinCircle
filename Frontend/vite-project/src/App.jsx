83075a3ce47704d5e25ba2689c52e5418808d034
lavstart <lawtripathi49@gmail.com>
2025-06-12 13:59:16 +0530

    tailwind setup

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -1,17 +1,18 @@
-import Navbar from "./components/Navbar";
-import BackgroundSection from "./components/BackGroundSection";
-import PopularCommunities from "./components/PopularCommunities";
+// import Navbar from "./components/Navbar";
+// import BackgroundSection from "./components/BackGroundSection";
+// import PopularCommunities from "./components/PopularCommunities";
 import AboutSection from "./components/AboutSection";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
 
 export default function HomePage() {
   return (
     <div className="text-gray-900 dark:text-white">
-      <Navbar />
-      <BackGround />
-      <PopularCommunities />
+      {/* <Navbar />
+     <BackgroundSection />
+
+      <PopularCommunities /> */}
       <AboutSection />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
