893f3261f449e3e846ee1ee0dcc49d94b52cc394
lavstart <lawtripathi49@gmail.com>
2025-06-13 11:06:18 +0530

    i have added background yet i will modify it later

---
+++ Frontend/vite-project/src/components/BackgroundSection.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const BackgroundSection = () => {
+  return (
+    <section className="min-h-screen p-12 text-center bg-blue-400 dark:bg-gray-700">
+        <h1>
+            Connect. Collaborate.Celebrate Campus Life.
+        </h1>
+        <h2>
+            join communities,share memes,plan outings,or ask for help-all in one place.
+        </h2>
+
+    </section>
+  )
+}
+
+export default BackgroundSection
